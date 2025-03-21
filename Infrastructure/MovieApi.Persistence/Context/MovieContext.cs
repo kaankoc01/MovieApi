3faf01210e1bcad44bc4b7d5f6bd75851488066c
kaan koc <kaankoc01@gmail.com>
2025-03-21 20:05:52 +0300

    db context yapısı

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(".\\SQLEXPRESS;initial Catalog=ApiMovieDb;integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
