48cca8bda23be1606db436cba9df44dad7c58646
kaan koc <kaankoc01@gmail.com>
2025-04-04 06:42:11 +0300

    Mediator tasarım deseninde Tag ve Cast işlemleri yapıldı

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/TagQueries/GetTagQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.TagResults;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries
+{
+    public class GetTagQuery : IRequest<List<GetTagQueryResult>>
+    {
+
+    }
+    
+}
---
+++ Presentation/MovieApi.WebApi/Controllers/CastsController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CastsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CastsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> CastList()
+        {
+            var result = await _mediator.Send(new GetCastQuery());
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCast(CreateCastCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Oyuncu ekleme işlemi tamamlandı.");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCast(int id)
+        {
+            await _mediator.Send(new RemoveCastCommand(id));
+            return Ok("Oyuncu silme işlemi tamamlandı.");
+        }
+        [HttpGet("GetCastById")]
+        public async Task<IActionResult> GetCastById(int id)
+        {
+            var result = await _mediator.Send(new GetCastByIdQuery(id));
+            return Ok(result);
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCast(UpdateCastCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Oyuncu güncelleme işlemi tamamlandı.");
+        }
+    }
+}
